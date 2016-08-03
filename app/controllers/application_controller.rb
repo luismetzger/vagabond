cf4b8dc9218649536df801e870da074479e8f50d
luismetzger <luismetzger@me.com>
2016-08-03 16:47:01 -0600

    Finished header, body of homepage and footer. Modal login and sign up are working.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
+  before_action  do
+    @user = User.new
+  end
+
   include SessionsHelper
+
+
 
 end
