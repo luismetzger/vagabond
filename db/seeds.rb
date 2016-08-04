887950aa695c509848837a6ad1fb7f5035d93aec
Alison Kenney <alisonkenney1@gmail.com>
2016-08-04 15:46:52 -0600

    reseeded database and added new welcome images

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,22 @@
 User.destroy_all
 Post.destroy_all
 
-user1 = User.create({
+city_1 = City.create({
+    name: "Denver",
+    photo: "http://d6jhp3hr7lf1v.cloudfront.net/wp-content/uploads/sites/106/2014/08/ss-21-denver-at-night-LUX.jpg"
+    })
+
+city_2 = City.create({
+    name: "San Francisco",
+    photo: "http://images.trvl-media.com/media/content/shared/images/travelguides/San-Francisco-and-vicinity-178305-smallTabletRetina.jpg"
+    })
+
+city_3 = City.create({
+    name: "Seattle",
+    photo: "http://www.trbimg.com/img-56ccbd2a/turbine/la-fi-mh-seattle-s-minimum-wage-kills-jobs-but-data-20160222"
+    })
+
+user_1 = User.create({
     first_name: "Alison",
     last_name: "Kenney",
     city: "Denver",
@@ -11,57 +26,88 @@ user1 = User.create({
     profile_pic: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAVVAAAAJDcyM2IyMDUyLTg2OTYtNGVlZC04ZTg2LWI5MjY1NDE0MmRkYw.jpg"
     })
 
-user2 = User.create({
+user_2 = User.create({
     first_name: "Nick",
     last_name: "Ursetta",
-    city: "Fort Collins",
+    city: "Denver",
     email: "nick@test.com",
     password: "nick",
     profile_pic: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/000/16f/3e1/15e36cb.jpg"
     })
 
+user_3 = User.create({
+    first_name: "Luis",
+    last_name: "Metzger",
+    city: "Denver",
+    email: "luis@test.com",
+    password: "luis",
+    profile_pic: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/000/2af/3fc/30c4495.jpg"
+    })
+
 post_1 = Post.create({
-    title: "This is a post",
-    description: "This is a description",
+    city_id: city_1.id, 
+    user_id: user_1.id,
+    title: "Cray YOLO Hoodie",
+    description: "Locavore next level man bun cold-pressed affogato. Godard taxidermy scenester, put a bird on it fingerstache mixtape cred. Tumblr tattooed slow-carb, gochujang XOXO health goth yuccie single-origin coffee street art viral chartreuse narwhal. Lomo food truck asymmetrical four loko, pork belly post-ironic before they sold out swag butcher tattooed shabby chic shoreditch ethical poutine offal.",
     })
 
 post_2 = Post.create({
-    title: "This is a post 2",
-    description: "This is a description 2",
+    city_id: city_1.id, 
+    user_id: user_1.id,
+    title: " iPhone Truffaut PBR&B",
+    description: "Raw denim vegan brooklyn, DIY poutine thundercats art party affogato bicycle rights dreamcatcher messenger bag salvia put a bird on it kogi. Gluten-free health goth aesthetic synth, paleo flannel four loko DIY +1 vice cred. Gluten-free leggings neutra literally hammock.",
     })
 
 post_3 = Post.create({
-    title: "This is a post 3",
-    description: "This is a description 3",
+    city_id: city_1.id, 
+    user_id: user_1.id,
+    title: "Hashtag Photo Booth",
+    description: "Fixie drinking vinegar vice shoreditch pabst, distillery cray. Fap organic keytar man bun banh mi occupy. Four dollar toast bushwick pinterest sustainable, leggings pork belly slow-carb brunch pug XOXO dreamcatcher typewriter. Master cleanse synth post-ironic viral meditation. Sriracha organic portland neutra blog mixtape narwhal.",
     })
 
 post_4 = Post.create({
-    title: "This is a post 4",
-    description: "This is a description 4",
+    city_id: city_2.id, 
+    user_id: user_2.id,
+    title: "XOXO Bespoke Trust Fund",
+    description: "You probably haven't heard of them meditation small batch, 8-bit irony yr stumptown dreamcatcher raw denim distillery affogato cardigan heirloom swag art party. Yr small batch kinfolk, taxidermy scenester wolf paleo art party mustache whatever.",
     })
 
 post_5 = Post.create({
-    title: "This is a post 5",
-    description: "This is a description 5",
+    city_id: city_2.id, 
+    user_id: user_2.id,
+    title: "Farm-To-Table Stumptown",
+    description: "Direct trade viral four loko typewriter cardigan. Portland four dollar toast banh mi, schlitz selfies iPhone truffaut PBR&B. Gentrify chartreuse hella, swag twee mustache banjo irony pickled roof party single-origin coffee tote bag church-key farm-to-table. Before they sold out schlitz swag, flexitarian locavore sriracha typewriter sartorial single-origin coffee shoreditch godard umami. ",
     })
 
 post_6 = Post.create({
-    title: "This is a post 6",
-    description: "This is a description 6",
+    city_id: city_2.id, 
+    user_id: user_2.id,
+    title: "Shabby Chic Lumbersexual",
+    description: "Gentrify viral vegan, artisan bitters intelligentsia poutine health goth lomo banjo tacos. Poutine williamsburg lo-fi, brooklyn gentrify knausgaard fanny pack listicle sustainable biodiesel helvetica 90's 8-bit you probably haven't heard of them authentic. Cronut banjo godard, keffiyeh chartreuse health goth salvia four loko cornhole freegan aesthetic. ",
     })
 
-city_1 = City.create({
-    name: "Denver",
-    photo: "http://housely.com/wp-content/uploads/2016/05/Denver.jpg"
+post_7 = Post.create({
+    city_id: city_3.id, 
+    user_id: user_3.id,
+    title: "XOXO Bespoke Trust Fund",
+    description: "You probably haven't heard of them meditation small batch, 8-bit irony yr stumptown dreamcatcher raw denim distillery affogato cardigan heirloom swag art party. Yr small batch kinfolk, taxidermy scenester wolf paleo art party mustache whatever.",
     })
 
-user1.posts << post_1
-user1.posts << post_2
-user1.posts << post_3
+post_8 = Post.create({
+    city_id: city_3.id, 
+    user_id: user_3.id,
+    title: "Farm-To-Table Stumptown",
+    description: "Direct trade viral four loko typewriter cardigan. Portland four dollar toast banh mi, schlitz selfies iPhone truffaut PBR&B. Gentrify chartreuse hella, swag twee mustache banjo irony pickled roof party single-origin coffee tote bag church-key farm-to-table. Before they sold out schlitz swag, flexitarian locavore sriracha typewriter sartorial single-origin coffee shoreditch godard umami. ",
+    })
 
-user2.posts << post_4
-user2.posts << post_5
-user2.posts << post_6
+post_9 = Post.create({
+    city_id: city_3.id, 
+    user_id: user_3.id,
+    title: "Shabby Chic Lumbersexual",
+    description: "Gentrify viral vegan, artisan bitters intelligentsia poutine health goth lomo banjo tacos. Poutine williamsburg lo-fi, brooklyn gentrify knausgaard fanny pack listicle sustainable biodiesel helvetica 90's 8-bit you probably haven't heard of them authentic. Cronut banjo godard, keffiyeh chartreuse health goth salvia four loko cornhole freegan aesthetic. ",
+    })
+
+
 
 
 
